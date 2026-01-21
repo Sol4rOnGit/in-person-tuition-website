69e43ba28c71747e890001cf9b01741d8def512f
Sol4rOnGit <Hiresh.Solanki@outlook.com>
2026-01-21 09:23:56 +0000

    Updated Bios
    Me & Prince bio updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,13 +21,13 @@ function About(){
                         <p className={styles.separation}>Founders</p>
                         <div className={styles.verticalCardContainer}>
                             <AboutCard name="Tanmaya Bharara" description="My expertise lies in Maths and physics which I am studying for A-Level (alongside Economics and Further Maths) at the London Academy of Excellence. Previously I have achieved all nines at GCSE in 11 subjects (99.98 percentile) and an A in Additional Maths and have tutored maths up to year 11/12."/>
-                            <AboutCard name="Hiresh Solanki" description="I specialise in computer science with almost a decade of coding experience, I built the very website you're looking at from the ground up. Dealing with admin and also tutoring computer science, I'll give your child the knowledge and skills needed to get top grades in computer science and mathematics."/>
+                            <AboutCard name="Hiresh Solanki" description="Specialises in computer science with almost a decade of coding experience, I built the very website you're looking at from the ground up, and I'll be giving your child the knowledge and skills needed to get top grades in computer science and mathematics."/>
                         </div>
                     </div>
                     <div>
                         <p className={styles.separation}>Staff</p>
                         <div className={styles.verticalCardContainer}>
-                            <AboutCard name="Prince Patel" description="Mathematical Genius. Aspiring Aerospace Engineer."/>
+                            <AboutCard name="Prince Patel" description="Aspiring Aerospace Engineer with a strong passion for mathematics, I'll bring your child to the next level in physics and mathematics."/>
                         </div>
                     </div>
                 </div>
